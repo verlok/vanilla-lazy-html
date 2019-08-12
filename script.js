95bec212b7d7f22457454eebb6ab652eec59fe3a
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2019-08-12 17:01:48 +0200

    Transpiled to ES5 for IE

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const lazyLoad = new LazyLoad({
-  elements_selector: ".lazy-dom",
-  callback_enter: (el) => {
-    const urlToCall = $(el).data('url');
-    $.ajax(urlToCall).done(asyncHtml => {
-      $(el).html(asyncHtml);
-    })
-  }
+var lazyLoad = new LazyLoad({
+	elements_selector: ".lazy-dom",
+	callback_enter: function(el) {
+		var urlToCall = $(el).data("url");
+		$.ajax(urlToCall).done(function(asyncHtml) {
+			$(el).html(asyncHtml);
+		});
+	}
 });
